a22d2941f8aacbb05addcf9e21326f1c8544d58d
Kovalenko Valeria <VKavalenka@ibagroup.eu>
2025-06-12 21:55:52 +0300

    [T02.01] Understanding the Basics 1

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Card from "./components/Card.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Card />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <h1>Song cards</h1>
+    </header>
+  );
+}
