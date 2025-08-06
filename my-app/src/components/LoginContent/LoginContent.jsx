ab75d74947576ee8f7542969c9f2da83485e098f
Kovalenko Valeria <VKavalenka@ibagroup.eu>
2025-08-06 15:48:18 +0300

    [T07.01] Working with Forms & User Input

--- my-app/src/components/LoginContent/LoginContent.jsx
+++ my-app/src/components/LoginContent/LoginContent.jsx
@@ -1,25 +1,79 @@
 import { Link } from 'react-router-dom';
 import './Login.css';
+import { useState } from 'react';
+import Input from './Input.jsx';
 
 export default function LoginContent() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [notValidPassword, setNotValidPassword] = useState(false);
+  const [disabledButton, setDisabledButton] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(enteredValues);
+    alert(JSON.stringify(enteredValues));
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues(prev => ({
+      ...prev,
+      [identifier]: value,
+    }));
+    setNotValidPassword(false);
+  }
+
+  function handleBlur() {
+    const passwordIsValid = /^(?=.*[a-zA-Z])(?=.*\d)/.test(enteredValues.password);
+
+    if (!passwordIsValid) {
+      setNotValidPassword(true);
+      setDisabledButton(true);
+    } else {
+      setDisabledButton(false);
+    }
+  }
+
   return (
     <main className="login-main">
-      <div>
+      <form onSubmit={handleSubmit}>
         <div className="login-fields">
-          <label for="login">Username </label>
-          <input type="text" id="login" />
+          <Input
+            label="Username"
+            id="email"
+            type="email"
+            value={enteredValues.email}
+            onChange={event => handleInputChange('email', event.target.value)}
+            required
+          />
         </div>
         <div className="login-fields">
-          <label for="password">Password</label>
-          <input type="password" id="password" />
+          <Input
+            label="Password"
+            id="password"
+            type="password"
+            onBlur={() => handleBlur()}
+            value={enteredValues.password}
+            onChange={event => handleInputChange('password', event.target.value)}
+            minLength={8}
+            required
+          />
         </div>
+        {notValidPassword ? (
+          <p className="not-valid">Password has to include at least 1 number and 1 letter</p>
+        ) : undefined}
         <div className="login-button">
-          <button>Login</button>
+          <button type="submit" disabled={disabledButton}>
+            Login
+          </button>
         </div>
         <div className="go-back">
           <Link to="/">Go to home page</Link>
         </div>
-      </div>
+      </form>
     </main>
   );
 }
